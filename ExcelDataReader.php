e10ea9db9873380da808b1cfb9fabecf07740e04
xyb <781028081>
2015-11-27 14:56:44 +0800

    init

---
+++ ExcelDataReader.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace xyb\phpexcel;
+
+class ExcelDataReader extends \yii\base\Object
+{
+    // TODO: implement excel to yii model reader
+    // - key (pk) param
+    // - update strategy (update, addIfNotExists)
+    // - transform func
+}
---
+++ ExcelTemplateWriter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace xyb\phpexcel;
+
+class ExcelTemplateWriter extends \yii\base\Object
+{
+    // TODO: implement excel template writer
+}
